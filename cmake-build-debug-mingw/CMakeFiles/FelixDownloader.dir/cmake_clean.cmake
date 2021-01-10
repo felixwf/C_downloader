d26e5bf90a857a395e21a7b76e09346e045c4469
felix <felix.wf@outlook.com>
2021-01-10 21:23:59 +0800

    modify CMakeLists.txt and conanfile for project config

---
+++ cmake-build-debug-mingw/CMakeFiles/FelixDownloader.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/FelixDownloader.dir/main.c.obj"
+  "FelixDownloader.exe"
+  "FelixDownloader.exe.manifest"
+  "FelixDownloader.pdb"
+  "libFelixDownloader.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/FelixDownloader.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
